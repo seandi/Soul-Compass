76ec6ef10077817c742e1f9774f0cc90515400da
seandi <eandi.simo@outlook.it>
2020-11-27 12:27:03 +0100

    Change test result bundle (consistent)

--- SoulCompassApp/app/src/main/java/com/example/soulcompass/ui/quiz/QuizResultFragment.java
+++ SoulCompassApp/app/src/main/java/com/example/soulcompass/ui/quiz/QuizResultFragment.java
@@ -16,15 +16,21 @@ import com.google.android.material.button.MaterialButton;
 
 public class QuizResultFragment extends Fragment {
 
-    private double result = 0.0;
+    private int result = 0;
+    private int scale = 1;
+    private double result_normalized = result;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_quiz_result, container, false);
-        result = getArguments().getDouble("Result");
-        Log.d("STRESS TEST", "Test result is " + String.valueOf(result) + "/1.0");
+
+        assert getArguments() != null;
+        result = getArguments().getInt("RESULT");
+        scale = getArguments().getInt("SCALE");
+        result_normalized = ((double) result) / ((double) scale);
+        Log.d("STRESS TEST", "Test result is " + String.valueOf(result_normalized) + "/1.0");
 
 
         return root;
