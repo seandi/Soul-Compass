7a489955050d4d7ebd32a3f5925029c1d572888b
seandi <eandi.simo@outlook.it>
2020-11-27 17:11:06 +0100

    Add item in toolbar to go back from settings/info to main activity

--- SoulCompassApp/app/src/main/java/com/example/soulcompass/SettingsActivity.java
+++ SoulCompassApp/app/src/main/java/com/example/soulcompass/SettingsActivity.java
@@ -2,13 +2,18 @@ package com.example.soulcompass;
 
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -24,5 +29,38 @@ public class SettingsActivity extends AppCompatActivity {
         setSupportActionBar(header_bar);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        getMenuInflater().inflate(R.menu.toolbar_settings_menu, menu);
+        Drawable drawable = menu.findItem(R.id.back).getIcon();
+
+        drawable = DrawableCompat.wrap(drawable);
+        DrawableCompat.setTint(drawable, ContextCompat.getColor(this,R.color.white));
+        menu.findItem(R.id.back).setIcon(drawable);
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.back) {
+            Intent back_intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(back_intent);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
