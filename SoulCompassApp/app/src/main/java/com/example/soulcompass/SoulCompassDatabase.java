bcc84e9ae25e043cf4cd28f843126b06db3e06ca
seandi <eandi.simo@outlook.it>
2020-12-09 18:16:40 +0100

    Add steps table and db logic

--- SoulCompassApp/app/src/main/java/com/example/soulcompass/SoulCompassDatabase.java
+++ SoulCompassApp/app/src/main/java/com/example/soulcompass/SoulCompassDatabase.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.widget.Toast;
 
 
 import androidx.core.util.Pair;
@@ -13,6 +14,7 @@ import androidx.core.util.Pair;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -22,7 +24,7 @@ import java.util.TreeMap;
 public class SoulCompassDatabase extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "SoulCompass";
+    public static final String DATABASE_NAME = "SoulCompass";
 
 
     // --------- TEST TABLE ------------------------------ //
@@ -45,6 +47,18 @@ public class SoulCompassDatabase extends SQLiteOpenHelper {
             UNLOCK_KEY_ID + " INTEGER PRIMARY KEY, " + UNLOCK_KEY_DATE + " TEXT);";
 
 
+    public static final String TABLE_NAME = "num_steps";
+    public static final String KEY_ID = "id";
+    public static final String KEY_TIMESTAMP = "timestamp";
+    public static final String KEY_HOUR = "hour";
+    public static final String KEY_DAY = "day";
+
+    public static final String CREATE_TABLE_SQL = "CREATE TABLE " + TABLE_NAME + " (" +
+            KEY_ID + " INTEGER PRIMARY KEY, " + KEY_DAY + " TEXT, " + KEY_HOUR + " TEXT, "
+            + KEY_TIMESTAMP + " TEXT);";
+
+
+
     private SimpleDateFormat dateFormat;
 
     public SoulCompassDatabase(Context context) {
@@ -59,6 +73,7 @@ public class SoulCompassDatabase extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TEST_TABLE_SQL);
         db.execSQL(CREATE_UNLOCK_TABLE_SQL);
+        db.execSQL(CREATE_TABLE_SQL);
         Log.d("DATABASE", "Databases created!");
     }
 
@@ -183,4 +198,166 @@ public class SoulCompassDatabase extends SQLiteOpenHelper {
         // 6. Return the map with hours and number of steps
         return map;
     }
+
+
+
+
+    /**
+     * Utility function to load all records in the database
+     *
+     * @param context: application context
+     */
+    public static void loadRecords(Context context){
+        List<String> dates = new LinkedList<String>();
+        SoulCompassDatabase databaseHelper = new SoulCompassDatabase(context);
+        SQLiteDatabase database = databaseHelper.getReadableDatabase();
+
+        String [] columns = new String [] {KEY_TIMESTAMP};
+        Cursor cursor = database.query(TABLE_NAME, columns, null, null, KEY_TIMESTAMP,
+                null, null );
+
+        // iterate over returned elements
+        cursor.moveToFirst();
+        for (int index=0; index < cursor.getCount(); index++){
+            dates.add(cursor.getString(0));
+            cursor.moveToNext();
+        }
+        database.close();
+
+        Log.d("STORED TIMESTAMPS: ", String.valueOf(dates));
+    }
+
+    /**
+     * Utility function to delete all records from the data base
+     *
+     * @param context: application context
+     */
+    public static void deleteRecords(Context context){
+        SoulCompassDatabase databaseHelper = new SoulCompassDatabase(context);
+        SQLiteDatabase database = databaseHelper.getWritableDatabase();
+        int numberDeletedRecords =0;
+
+        numberDeletedRecords = database.delete(TABLE_NAME, null, null);
+        database.close();
+
+        // display the number of deleted records with a Toast message
+        Toast.makeText(context,"Deleted " + String.valueOf(numberDeletedRecords) + " steps",Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * Utility function to load records from a single day
+     *
+     * @param context: application context
+     * @param date: today's date
+     * @return numSteps: an integer value with the number of records in the database
+     */
+    //
+    public static Integer loadSingleRecord(Context context, String date){
+        List<String> steps = new LinkedList<String>();
+        // Get the readable database
+        SoulCompassDatabase databaseHelper = new SoulCompassDatabase(context);
+        SQLiteDatabase database = databaseHelper.getReadableDatabase();
+
+        String where = KEY_DAY + " = ?";
+        String [] whereArgs = { date };
+
+        Cursor cursor = database.query(TABLE_NAME, null, where, whereArgs, null,
+                null, null );
+
+        // iterate over returned elements
+        cursor.moveToFirst();
+        for (int index=0; index < cursor.getCount(); index++){
+            steps.add(cursor.getString(0));
+            cursor.moveToNext();
+        }
+        database.close();
+
+        Integer numSteps = steps.size();
+        Log.d("STORED STEPS TODAY: ", String.valueOf(numSteps));
+        return numSteps;
+    }
+
+    /**
+     * Utility function to get the number of steps by hour for current date
+     *
+     * @param context: application context
+     * @param date: today's date
+     * @return map: map with key-value pairs hour->number of steps
+     */
+    //
+    public static Map<Integer, Integer> loadStepsByHour(Context context, String date){
+        // 1. Define a map to store the hour and number of steps as key-value pairs
+        Map<Integer, Integer>  map = new HashMap<> ();
+
+        // 2. Get the readable database
+        SoulCompassDatabase databaseHelper = new SoulCompassDatabase(context);
+        SQLiteDatabase database = databaseHelper.getReadableDatabase();
+
+        // 3. Define the query to get the data
+        Cursor cursor = database.rawQuery("SELECT hour, COUNT(*)  FROM num_steps " +
+                "WHERE day = ? GROUP BY hour ORDER BY  hour ASC ", new String [] {date});
+
+        // 4. Iterate over returned elements on the cursor
+        cursor.moveToFirst();
+        for (int index=0; index < cursor.getCount(); index++){
+            Integer tmpKey = Integer.parseInt(cursor.getString(0));
+            Integer tmpValue = Integer.parseInt(cursor.getString(1));
+
+            //2. Put the data from the database into the map
+            map.put(tmpKey, tmpValue);
+
+
+            cursor.moveToNext();
+        }
+
+        // 5. Close the cursor and database
+        cursor.close();
+        database.close();
+
+        // 6. Return the map with hours and number of steps
+        return map;
+    }
+
+    /**
+     * Utility function to get the number of steps by day
+     *
+     * @param context: application context
+     * @return map: map with key-value pairs hour->number of steps
+     */
+    //
+    public static Map<String, Integer> loadStepsByDay(Context context){
+        // 1. Define a map to store the hour and number of steps as key-value pairs
+        Map<String, Integer>  map = new TreeMap<>();
+
+        // 2. Get the readable database
+        SoulCompassDatabase databaseHelper = new SoulCompassDatabase(context);
+        SQLiteDatabase database = databaseHelper.getReadableDatabase();
+
+        // 3. Define the query to get the data
+        Cursor cursor = database.rawQuery("SELECT day, COUNT(*)  FROM num_steps " +
+                "GROUP BY day ORDER BY day ASC ", new String [] {});
+
+        // 4. Iterate over returned elements on the cursor
+        cursor.moveToFirst();
+        for (int index=0; index < cursor.getCount(); index++){
+            String tmpKey = cursor.getString(0);
+            Integer tmpValue = Integer.parseInt(cursor.getString(1));
+
+            // Put the data from the database into the map
+            map.put(tmpKey, tmpValue);
+            cursor.moveToNext();
+        }
+
+        // 5. Close the cursor and database
+        cursor.close();
+        database.close();
+
+        // 6. Return the map with hours and number of steps
+        return map;
+    }
+
+
+
+
+
 }
